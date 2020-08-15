3ee3b2770d129847a8901b52d1d8d96f43295cd8
hslam <791874158@qq.com>
2020-08-15 14:40:54 +0800

    add copyright

--- mmap.go
+++ mmap.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2020 Meng Huang (mhboy@outlook.com)
+// This package is licensed under a MIT license that can be found in the LICENSE file.
+
+// Package mmap provides a way to memory-map a file.
 package mmap
 
 import (
